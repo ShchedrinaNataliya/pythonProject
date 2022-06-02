b8daa04ae79e60aa43e7e8ae7dd2e651af41c7da
nataliya <shchedrinanataliya@gmail.com>
2022-06-02 23:47:34 +0300

    Initial commit

---
+++ Shchedrina_Natalia_dz3/Task1.py
@@ -0,0 +1,20 @@
+find_element = 'three'
+
+num_translate = {
+    'zero': 'ноль',
+    'one': 'один',
+    'two': 'два',
+    'three': 'три',
+    'four': 'четыре',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'nine': 'девять',
+    'ten': 'десять',
+
+}
+
+res = num_translate.get(find_element, None)
+
+print(res)
